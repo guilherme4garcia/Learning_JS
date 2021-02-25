7d6b17542cb55d57c25987be9f0f0e15022ef33e
Guilherme Garcia <guilherme4garcia@gmail.com>
2021-02-25 15:16:01 -0300

    upload

---
+++ Exercicios_JS/Fundamentos/funcao_em_quase_tudo.js
@@ -0,0 +1,6 @@
+console.log(typeof Object) // Objetos em JS são funções
+
+class Produto {}
+console.log(typeof Produto) // Classes em JS são funções 
+
+//JavaScript é altamente centrado em funções.
