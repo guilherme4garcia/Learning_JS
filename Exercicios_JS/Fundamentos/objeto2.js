528261638b62eee3c1988df838be2b9f27cd2f89
Guilherme Garcia <guilherme4garcia@gmail.com>
2021-02-26 12:40:03 -0300

    upload

---
+++ Exercicios_JS/Fundamentos/objeto2.js
@@ -0,0 +1,9 @@
+console.log(typeof Object)
+console.log(typeof new Object)
+
+const Client = function() {}
+console.log(typeof Cliente)
+console.log(typeof new Client)
+
+class Produto {} // ES 2015
+console.log(typeof Produto)
